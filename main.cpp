0d099740b684940a5a6726d9207590cd9ffa9123
Kotsag Sofiya <gsophiay@gmail.com>
2023-06-05 20:17:33 +0300

    20.27 pipi

--- main.cpp
+++ main.cpp
@@ -2,42 +2,40 @@
 #define SIZE 5
 using namespace std;
 
-int** init_array(int size)
+//INIT ARRAY
+int** initArray(int** arr, int size)
 {
     srand(time(0));
-    int** arr = new int* [size];
     for (size_t i = 0; i < size; i++)
     {
         arr[i] = new int[size];
-        for (size_t j = 0; j < SIZE; j++)
+        for (size_t j = 0; j < size; j++)
         {
             arr[i][j] = rand() % 10;
         }
     }
     return arr;
 }
-
-double** init_array(double** arr)
+double** initArray(double** arr, int size)
 {
     srand(time(0));
-    for (size_t i = 0; i < SIZE; i++)
+    for (size_t i = 0; i < size; i++)
     {
-        arr[i] = new double[SIZE];
-        for (size_t j = 0; j < SIZE; j++)
+        arr[i] = new double[size];
+        for (size_t j = 0; j < size; j++)
         {
             arr[i][j] = rand() % 10;
         }
     }
     return arr;
 }
-
-char** init_array(char** arr)
+char** initArray(char** arr, int size)
 {
     srand(time(0));
-    for (size_t i = 0; i < SIZE; i++)
+    for (size_t i = 0; i < size; i++)
     {
-        arr[i] = new char[SIZE];
-        for (size_t j = 0; j < SIZE; j++)
+        arr[i] = new char[size];
+        for (size_t j = 0; j < size; j++)
         {
             arr[i][j] = rand() % 10;
         }
@@ -47,69 +45,164 @@ char** init_array(char** arr)
 
 
 //PRINT ARRAY
-
-void print_array(int** arr)
+void printArray(int** arr, int size)
 {
-    for (size_t i = 0; i < 200; i++)
+    for (size_t i = 0; i < size; i++)
     {
-        for (size_t j = 0; j < 200; j++)
+        for (size_t j = 0; j < size; j++)
         {
-            if (arr[i][j] != -33686019)
-            {
-                cout << arr[i][j] << " ";
-            }
-            else
-            {
-                break;
-            }
+            cout << arr[i][j] << " ";
         }
         cout << endl;
-    } 
+    }
 }
-
-void print_array(double** arr)
+void printArray(double** arr, int size)
 {
-    for (size_t i = 0; i < 200; i++)
+    for (size_t i = 0; i < size; i++)
     {
-        for (size_t j = 0; j < 200; j++)
+        for (size_t j = 0; j < size; j++)
         {
-            if (arr[i][j] != -33686019)
-            {
-                cout << arr[i][j] << " ";
-            }
-            else
-            {
-                break;
-            }
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+void printArray(char** arr, int size)
+{
+    for (size_t i = 0; i < size; i++)
+    {
+        for (size_t j = 0; j < size; j++)
+        {
+            cout << arr[i][j] << " ";
         }
         cout << endl;
     }
 }
 
-void print_array(char** arr)
+//MAX MAIN DIAGONAL ARRAY
+int maxMainDiagonal(int** arr, int size)
 {
-    for (size_t i = 0; i < 200; i++)
-    {
-        for (size_t j = 0; j < 200; j++)
-        {
-            if (arr[i][j] != -33686019)
-            {
-                cout << arr[i][j] << " ";
-            }
-            else
-            {
-                break;
+    int max = 0;
+    for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (i == j/* || i + j == size - 1*/)
+                {
+                    if (arr[i][j] > max)
+                    {
+                        max = arr[i][j];
+                    }
+                }                  
             }
         }
-        cout << endl;
-    }
-    cout << endl;
+    return max;
 }
+double maxMainDiagonal(double** arr, int size)
+{
+    double max = 0;
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            if (i == j || i + j == size - 1)
+            {
+                if (arr[i][j] > max)
+                {
+                    max = arr[i][j];
+                }
+            }
+        }
+    }
+    return max;
+}
+char maxMainDiagonal(char** arr, int size)
+{
+    char max = 0;
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            if (i == j || i + j == size - 1)
+            {
+                if (arr[i][j] > max)
+                {
+                    max = arr[i][j];
+                }
+            }
+        }
+    }
+    return max;
+}
+
+//MIN MAIN DIAGONAL ARRAY
+int minMainDiagonal(int** arr, int size)
+{
+    int min = 10;
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            if (i == j/* || i + j == size - 1*/)
+            {
+                if (arr[i][j] < min)
+                {
+                    min = arr[i][j];
+                }
+            }
+        }
+    }
+    return min;
+}
+double minMainDiagonal(double** arr, int size)
+{
+    double min = 10;
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            if (i == j || i + j == size - 1)
+            {
+                if (arr[i][j] < min)
+                {
+                    min = arr[i][j];
+                }
+            }            
+        }
+    }
+    return min;
+}
+char minMainDiagonal(char** arr, int size)
+{
+    char min = 10;
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            if (i == j || i + j == size - 1)
+            {
+                if (arr[i][j] < min)
+                {
+                    min = arr[i][j];
+                }
+            }
+        }
+    }
+    return min;
+}
+
+
 int main()
 {
     int size = 5;
-    int** arr = init_array(size);
-    print_array(arr);
+    int** arr = new int* [size];
+    arr = initArray(arr, size);
+    cout << "Print array: " << endl;
+    printArray(arr, size);
+    cout << "Main diagonal max: ";
+    int max = maxMainDiagonal(arr, size);
+    cout << max << endl;
+    cout << "Main diagonal min: ";
+    int min = minMainDiagonal(arr, size);
+    cout << min << endl;
+
+
+
+
+
+
+
+
+
+
 
 
 
